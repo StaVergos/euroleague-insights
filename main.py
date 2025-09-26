2d03611497b59897b85506e194311384c4cdc490
StaVergos <stavrospvergos@gmail.com>
2025-09-26 21:39:08 +0300

    Added get clubs

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from src.euroleague.schemas.clubs import DTOClub
+from src.euroleague.service import EuroleagueService
 
 app = FastAPI()
+
 
 @app.get("/health")
 def healthcheck():
     return {"status": "ok"}
 
+
 @app.get("/")
 def main():
     return "Hello World"
+
+
+@app.get("/clubs")
+def get_clubs(euroleague_service=Depends(EuroleagueService)) -> list[DTOClub]:
+    return euroleague_service.get_clubs()
