5e9070248aefa9249092370e2bf1896f6eee5fd3
G.Perdikaris <perdgeorge@gmail.com>
2025-12-16 15:04:52 +0200

    Import player model

--- euroleague_insights/euroleague/models.py
+++ euroleague_insights/euroleague/models.py
@@ -13,3 +13,20 @@ class Club(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Player(models.Model):
+    id = models.AutoField(primary_key=True)
+    code = models.CharField(max_length=10, unique=True)
+    fullname = models.CharField(max_length=50)
+    passport_name = models.CharField(max_length=50, blank=True)
+    passport_surname = models.CharField(max_length=50, blank=True)
+    jersey_name = models.CharField(max_length=50, blank=True)
+    country_code = models.CharField(max_length=3)
+    country_name = models.CharField(max_length=100)
+    height = models.IntegerField(nullable=True)
+    weight = models.IntegerField(nullable=True)
+    birth_date = models.DateField(blank=True)
+
+    def __str__(self):
+        return self.fullname
