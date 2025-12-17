b61992f1f64850649eaf7b46053c041bbb64fa51
G.Perdikaris <perdgeorge@gmail.com>
2025-12-17 15:30:07 +0200

    Add player serializer

--- euroleague_insights/euroleague/serializers.py
+++ euroleague_insights/euroleague/serializers.py
@@ -10,3 +10,17 @@ class ClubSerializer(serializers.Serializer):
     country_name = serializers.CharField(max_length=100)
     address = serializers.CharField(max_length=255)
     city = serializers.CharField(max_length=100)
+
+
+class PlayerSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    code = serializers.CharField(max_length=10)
+    fullname = serializers.CharField(max_length=50)
+    passport_name = serializers.CharField(max_length=50)
+    passport_surname = serializers.CharField(max_length=50)
+    jersey_name = serializers.CharField(max_length=50)
+    country_code = serializers.CharField(max_length=3)
+    country_name = serializers.CharField(max_length=100)
+    height = serializers.IntegerField()
+    weight = serializers.IntegerField()
+    birth_date = serializers.DateTimeField()
