9b38671926166ef17abe2608a0cdf74da1156815
G.Perdikaris <perdgeorge@gmail.com>
2025-12-17 15:34:01 +0200

    Add players path

--- euroleague_insights/euroleague/urls.py
+++ euroleague_insights/euroleague/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from euroleague_insights.euroleague.views import ListClubsView
+from euroleague_insights.euroleague.views import ListPlayersView
 
 urlpatterns = [
     path("clubs/", ListClubsView.as_view(), name="list-clubs"),
+    path("players/", ListPlayersView.as_view(), name="list-players"),
 ]
