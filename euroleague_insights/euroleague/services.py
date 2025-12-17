7a6b7704bd0e86a1c5b330091aebd17fc2f48de9
G.Perdikaris <perdgeorge@gmail.com>
2025-12-17 15:33:31 +0200

    add list_players

--- euroleague_insights/euroleague/services.py
+++ euroleague_insights/euroleague/services.py
@@ -1,10 +1,14 @@
 import logging
 
 from euroleague_insights.euroleague.models import Club
-from euroleague_insights.euroleague.tasks import insert_clubs
+from euroleague_insights.euroleague.models import Player
 
 logger = logging.getLogger(__name__)
 
 
 def list_clubs():
     return Club.objects.all()
+
+
+def list_players():
+    return Player.objects.all()
