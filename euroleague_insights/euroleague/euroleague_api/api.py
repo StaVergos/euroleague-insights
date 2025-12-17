20c8a7a6c45696ea8f446eaa50648eed1d27d996
G.Perdikaris <perdgeorge@gmail.com>
2025-12-17 19:15:11 +0200

    update api

--- euroleague_insights/euroleague/euroleague_api/api.py
+++ euroleague_insights/euroleague/euroleague_api/api.py
@@ -6,7 +6,8 @@ from euroleague_insights.euroleague.euroleague_api.constants import SeasonCode
 
 
 class EuroleagueAPI:
-    def __init__(self):
+    def __init__(self, season: str):
+        self.season = SeasonCode(season)
         self.client = httpx.Client()
         self.live_client = httpx.Client()
 
@@ -14,33 +15,33 @@ class EuroleagueAPI:
     def api_v2(self) -> str:
         return EuroleagueApiUrl.V2.value
 
-    def euroleague_2024_games_url(self, season_code: SeasonCode) -> str:
+    def euroleague_2024_games_url(self) -> str:
         return (
             f"{self.api_v2}"
             f"/competitions/{CompetitionCode.EUROLEAGUE.value}"
-            f"/seasons/{season_code.value}/games"
+            f"/seasons/{self.season.value}/games"
         )
 
-    def euroleague_clubs_url(self, season_code: SeasonCode) -> str:
+    def euroleague_clubs_url(self) -> str:
         return (
             f"{self.api_v2}"
             f"/competitions/{CompetitionCode.EUROLEAGUE.value}"
-            f"/seasons/{season_code.value}/clubs"
+            f"/seasons/{self.season.value}/clubs"
         )
 
-    def euroleague_players_url(self, season_code: SeasonCode) -> str:
+    def euroleague_players_url(self) -> str:
         return (
             f"{self.api_v2}"
             f"/competitions/{CompetitionCode.EUROLEAGUE.value}"
-            f"/seasons/{season_code.value}/people"
+            f"/seasons/{self.season.value}/people"
         )
 
-    def get_clubs(self, season: SeasonCode):
-        response = self.client.get(self.euroleague_clubs_url(season))
+    def get_clubs(self):
+        response = self.client.get(self.euroleague_clubs_url())
         response.raise_for_status()
         return response.json()
 
-    def get_players(self, season: SeasonCode):
-        response = self.client.get(self.euroleague_players_url(season))
+    def get_players(self):
+        response = self.client.get(self.euroleague_players_url())
         response.raise_for_status()
         return response.json()
