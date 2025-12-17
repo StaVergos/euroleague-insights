7de595ac6a04418467ab73cb11f02c4170fddf91
G.Perdikaris <perdgeorge@gmail.com>
2025-12-17 12:21:07 +0200

    Import insert_player task

--- euroleague_insights/euroleague/tasks.py
+++ euroleague_insights/euroleague/tasks.py
@@ -5,6 +5,7 @@ from celery import shared_task
 from euroleague_insights.euroleague.euroleague_api.api import EuroleagueAPI
 from euroleague_insights.euroleague.euroleague_api.constants import SeasonCode
 from euroleague_insights.euroleague.models import Club
+from euroleague_insights.euroleague.models import Player
 
 logger = logging.getLogger(__name__)
 
@@ -29,5 +30,32 @@ def insert_clubs(season_code):
                 "country_name": country_name,
                 "address": club_datum.get("address", ""),
                 "city": club_datum.get("city", ""),
+            },
+        )
+
+
+@shared_task()
+def insert_players(season_code):
+    api = EuroleagueAPI()
+    season = SeasonCode(season_code)
+    players_data = api.get_players(season).get("data", [])
+    logger.info("Fetched %d players for season %s", len(players_data), season_code)
+    for new_player in players_data:
+        logger.info("Processing player: %s", new_player)
+        new_player_person = new_player.get("person", {})
+        country = new_player_person.get("country") or {}
+        country_code = country.get("code", "")
+        country_name = country.get("name", "")
+        Player.objects.update_or_create(
+            default={
+                "fullname": new_player_person.get("name", ""),
+                "passport_name": new_player_person.get("passportName", ""),
+                "passport_surname": new_player_person.get("passportSurname", ""),
+                "jersey_name": new_player_person.get("jerseyName", ""),
+                "country_code": country_code,
+                "country_name": country_name,
+                "height": new_player_person.get("height", None),
+                "weight": new_player_person.get("weight", None),
+                "birth_date": new_player_person.get("birthDate", ""),
             },
         )
