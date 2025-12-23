08341fd4bf1d4b2ca6ff2c28f45a68d964cb4bd2
G.Perdikaris <perdgeorge@gmail.com>
2025-12-23 20:15:39 +0200

    Transform serializer into modelserializer

--- euroleague_insights/euroleague/serializers.py
+++ euroleague_insights/euroleague/serializers.py
@@ -1,26 +1,41 @@
 from rest_framework import serializers
+from euroleague_insights.euroleague.models import Player, Club
 
 
-class ClubSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    code = serializers.CharField(max_length=10)
-    name = serializers.CharField(max_length=255)
-    alias = serializers.CharField(max_length=100)
-    country_code = serializers.CharField(max_length=3)
-    country_name = serializers.CharField(max_length=100)
-    address = serializers.CharField(max_length=255)
-    city = serializers.CharField(max_length=100)
+class ClubSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = (
+            "id",
+            "code",
+            "name",
+            "alias",
+            "country_code",
+            "country_name",
+            "address",
+            "city",
+        )
 
 
-class PlayerSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    code = serializers.CharField(max_length=10)
-    fullname = serializers.CharField(max_length=50)
-    passport_name = serializers.CharField(max_length=50)
-    passport_surname = serializers.CharField(max_length=50)
-    jersey_name = serializers.CharField(max_length=50)
-    country_code = serializers.CharField(max_length=3)
-    country_name = serializers.CharField(max_length=100)
-    height = serializers.IntegerField()
-    weight = serializers.IntegerField()
-    birth_date = serializers.DateTimeField()
+class PlayerSerializer(serializers.ModelSerializer):
+    current_club = serializers.CharField(
+        source="current_club.name", read_only=True, allow_blank=True, allow_null=True
+    )
+
+    class Meta:
+        model = Player
+        fields = (
+            "id",
+            "code",
+            "fullname",
+            "passport_name",
+            "passport_surname",
+            "jersey_name",
+            "country_code",
+            "country_name",
+            "height",
+            "weight",
+            "birth_date",
+            "current_club",
+        )
+        read_only_fields = ("id", "current_club")
