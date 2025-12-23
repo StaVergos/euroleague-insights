d70ec32c7a1d4b26d7e0a086e0647e817ed4f50d
G.Perdikaris <perdgeorge@gmail.com>
2025-12-23 21:18:08 +0200

    update url for player details

--- euroleague_insights/euroleague/euroleague_api/api.py
+++ euroleague_insights/euroleague/euroleague_api/api.py
@@ -46,11 +46,11 @@ class EuroleagueAPI:
         response.raise_for_status()
         return response.json()
 
-    def get_player_details_url(self, person_code: str):
+    def get_player_details_url(self):
         url = (
             f"{self.api_v2}"
             f"/competitions/{CompetitionCode.EUROLEAGUE.value}"
-            f"/people/{person_code}"
+            f"/seasons/{self.season.value}"
         )
         response = self.client.get(url)
         response.raise_for_status()
