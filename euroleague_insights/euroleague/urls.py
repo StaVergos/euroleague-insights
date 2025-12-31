5bbb8be1ce7c493785ba0e95433ef68013e3d7d5
G.Perdikaris <perdgeorge@gmail.com>
2025-12-31 21:37:02 +0200

    Update urls for club players

--- euroleague_insights/euroleague/urls.py
+++ euroleague_insights/euroleague/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from euroleague_insights.euroleague.views import ListClubsView
+from euroleague_insights.euroleague.views import ListClubPlayersView, ListClubsView
 from euroleague_insights.euroleague.views import ListPlayersView
 
 urlpatterns = [
     path("clubs/", ListClubsView.as_view(), name="list-clubs"),
     path("players/", ListPlayersView.as_view(), name="list-players"),
+    path(
+        "clubs/<str:code>/players/",
+        ListClubPlayersView.as_view(),
+        name="list-club-players",
+    ),
 ]
