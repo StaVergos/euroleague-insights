15e009c001f7f44a7cada6e1cf6d063960a2a247
G.Perdikaris <perdgeorge@gmail.com>
2025-12-31 21:49:31 +0200

    Add list_club_players

--- euroleague_insights/euroleague/services.py
+++ euroleague_insights/euroleague/services.py
@@ -12,3 +12,13 @@ def list_clubs():
 
 def list_players():
     return Player.objects.order_by("id")
+
+
+def list_club_players(code):
+    try:
+        club = Club.objects.get(code=code)
+    except Club.DoesNotExist:
+        logger.exception("Club with code %s does not exist", code)
+        return Player.objects.none()
+
+    return Player.objects.filter(current_club=club).order_by("id")
