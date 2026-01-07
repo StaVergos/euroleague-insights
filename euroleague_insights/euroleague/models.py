4f061b7272fbba89d82f798d366feb1c337cdf7b
G.Perdikaris <perdgeorge@gmail.com>
2026-01-07 17:51:35 +0200

    Add Play model

--- euroleague_insights/euroleague/models.py
+++ euroleague_insights/euroleague/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 from euroleague_insights.euroleague.euroleague_api.constants import PhaseType
+from euroleague_insights.euroleague.euroleague_api.constants import PlayType
+from euroleague_insights.euroleague.euroleague_api.constants import Quarter
 
 
 class Club(models.Model):
@@ -71,3 +73,48 @@ class Match(models.Model):
     def set_phase_type(self, phase_type):
         if isinstance(phase_type, PhaseType):
             self.phase = phase_type.value
+
+
+PLAYTYPE_CHOICES = [(pt.value, pt.value) for pt in PlayType]
+QUARTER_CHOICES = [(q.value, q.value) for q in Quarter]
+
+
+class Play(models.Model):
+    id = models.AutoField(primary_key=True)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name="team_play")
+    quarter = models.CharField(
+        max_length=20,
+        choices=QUARTER_CHOICES,
+        default=Quarter.FIRST.value,
+    )
+    number_of_play = models.IntegerField(unique=True)
+    player = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True)
+    play_team = models.CharField(max_length=50)
+    play_type = models.CharField(
+        max_length=20,
+        choices=PLAYTYPE_CHOICES,
+        default=PlayType.BEGIN_PERIOD.value,
+    )
+    marker_time = models.TimeField(blank=True)
+    home_team_play_points = models.IntegerField(null=True, default=None)
+    away_team_play_points = models.IntegerField(null=True, default=None)
+    play_info = models.CharField(max_length=100, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["match", "number_of_play"],
+                name="unique_play_per_match",
+            ),
+        ]
+
+    def __str__(self):
+        return f"Play {self.number_of_play} in Match {self.match.game_code}"
+
+    def set_play_type(self, play_type):
+        if isinstance(play_type, PlayType):
+            self.play_type = play_type.value
+
+    def set_quarter(self, quarter):
+        if isinstance(quarter, Quarter):
+            self.quarter = quarter.value
