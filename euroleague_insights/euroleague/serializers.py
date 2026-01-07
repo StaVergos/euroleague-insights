26d5686b7d4ead1abec8f63f9148a83bf260b7de
G.Perdikaris <perdgeorge@gmail.com>
2026-01-07 17:53:40 +0200

    Add Play serializer

--- euroleague_insights/euroleague/serializers.py
+++ euroleague_insights/euroleague/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from euroleague_insights.euroleague.euroleague_api.constants import PhaseType
+from euroleague_insights.euroleague.euroleague_api.constants import PlayType
+from euroleague_insights.euroleague.euroleague_api.constants import Quarter
+from euroleague_insights.euroleague.models import Play
 
 
 class ClubSerializer(serializers.Serializer):
@@ -46,3 +50,19 @@ class MatchSerializer(serializers.Serializer):
     away_score = serializers.IntegerField(allow_null=True, default=None)
     venue_name = serializers.CharField(max_length=100)
     audience = serializers.IntegerField(allow_null=True, default=None)
+
+
+class PlaySerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    match = serializers.CharField()
+    quarter = serializers.ChoiceField(choices=[(q.value, q.value) for q in Quarter])
+    number_of_play = serializers.IntegerField()
+    player = serializers.CharField(max_length=50)
+    play_team = serializers.CharField(max_length=50)
+    play_type = serializers.ChoiceField(
+        choices=[(pt.value, pt.value) for pt in PlayType],
+    )
+    marker_time = serializers.TimeField()
+    home_team_play_points = serializers.IntegerField(allow_null=True, default=None)
+    away_team_play_points = serializers.IntegerField(allow_null=True, default=None)
+    play_info = serializers.CharField(max_length=100)
